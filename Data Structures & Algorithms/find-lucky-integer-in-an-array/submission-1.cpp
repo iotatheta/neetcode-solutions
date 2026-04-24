fccaa64fa1bd88be1cc672df70c8937d52fa244c
iotatheta <princebiswas2006@gmail.com>
2026-04-25 00:05:00 +0530

    Add: find-lucky-integer-in-an-array - submission-1

---
+++ Data Structures & Algorithms/find-lucky-integer-in-an-array/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) 
+    {
+        int maxi=-1;
+        unordered_map<int,int> mp;
+        for(int i=0;i<arr.size();i++)
+        {
+            mp[arr[i]]++;
+        }    
+        for(auto it : mp)
+        {
+            if(it.first==it.second)
+                maxi=max(maxi,it.first);
+        }
+        return maxi;
+    }
+};

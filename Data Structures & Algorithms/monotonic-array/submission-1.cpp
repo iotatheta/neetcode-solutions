2f14f1ab75d729df4241abe70b1b2d2fa836fd57
iotatheta <princebiswas2006@gmail.com>
2026-04-26 00:09:43 +0530

    Add: monotonic-array - submission-1

---
+++ Data Structures & Algorithms/monotonic-array/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    bool isSorted(vector<int>& arr)
+    {
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i]<arr[i-1])
+                return false;
+        }
+        return true;
+    }
+public:
+    bool isMonotonic(vector<int>& nums) 
+    {
+        int cnt1=1,cnt2=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]<=nums[i-1])
+                cnt1++;
+            if(nums[i]>=nums[i-1])
+                cnt2++;
+        }
+        return cnt1==nums.size() | cnt2==nums.size();    
+    }
+};

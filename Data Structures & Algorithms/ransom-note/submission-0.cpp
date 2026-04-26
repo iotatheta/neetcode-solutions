cbbbf7526c0d5f4b13769a4cba31ca895a8730e4
iotatheta <princebiswas2006@gmail.com>
2026-04-26 20:35:14 +0530

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+        vector<int> freq(26,0);
+        for(int i=0;i<magazine.size();i++)
+        {
+            freq[magazine[i]-'a']++;
+        }   
+        for(int i=0;i<ransomNote.size();i++)
+        {
+            freq[ransomNote[i]-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]<0) return false;
+        }
+        return true;
+    }
+};
